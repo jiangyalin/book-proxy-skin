2b53e12c5d4e4f45c95ab5d097a6825fa53ce6a3
yalin.jiang@asapone.cn <995991>
2021-04-02 16:34:47 +0800

    init

---
+++ config/default.js
@@ -0,0 +1,10 @@
+//mongodb配置文件
+module.exports = {
+  port: 8087,
+  session: {
+    secret: 'personalDB',
+    key: 'personalDB',
+    maxAge: 2592000000
+  },
+  mongodb: 'mongodb://localhost:27017/personalDB'
+}
---
+++ server.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const pkg = require('./package')
+const routes = require('./routes/')
+const config = require('./config/default')
+const bodyParser = require('body-parser')
+
+app.set('views', path.join(__dirname, 'views'))
+app.engine('html', require('ejs').renderFile) // 设置模板后缀名为.html
+app.set('view engine', 'html')
+
+app.use(express.static(path.join(__dirname, 'static')))
+
+app.locals.blog = {
+  title: pkg.name,
+  description: pkg.description
+}
+
+app.use(bodyParser.json({ limit: '50mb' })) // 设置最大提交值
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+app.use((req, res, next) => {
+  res.locals.user = req.session.user
+  next()
+})
+
+app.all('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild')
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
+
+  if (req.method === 'OPTIONS') {
+    res.send(200)
+  } else {
+    next()
+  }
+})
+
+routes(app)
+
+app.listen(config.port, () => {
+  console.log('服务启动' + config.port)
+})
+
