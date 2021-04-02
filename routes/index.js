2a6cbe084823c0d685966d516c2d26e5aa47f97b
yalin.jiang@asapone.cn <995991>
2021-04-02 17:25:57 +0800

    init

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 const bodyParser = require('body-parser')
 
-module.exports = (app) => {
+module.exports = app => {
 
   app.use(bodyParser.urlencoded({ extended: false }))
   app.use(bodyParser.json()) // 解析参数
 
   app.get('/', (req, res) => {
-    console.log('ppp')
     res.render('index', { data: {} })
   })
 }
