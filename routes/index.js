a970d95785427b3c9f9f5da5c8390efc3070eed6
yalin.jiang@asapone.cn <995991>
2021-04-07 10:18:01 +0800

    get list

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,55 @@ module.exports = app => {
   app.use(bodyParser.json()) // 解析参数
 
   app.get('/', (req, res) => {
-    console.log('pp')
-    getList(data => {
-      res.render('index', { data })
-    })
+    // getList(data => {
+    //   res.render('index', { data })
+    // })
+    res.render('index', { data: [{
+        title: '这是标题',
+        href: 'href',
+        cover: 'http://124.70.153.221:9998/ftppic/2021/20210305100759585.jpg',
+        page: 5,
+        createTime: '2021-02-02',
+        author: '这是作者',
+        bookName: '这是书名',
+        isNotFix: 'false'
+      }, {
+        title: '这是标题',
+        href: 'href',
+        cover: 'http://124.70.153.221:9998/ftppic/2021/20210305100759585.jpg',
+        page: 5,
+        createTime: '2021-02-02',
+        author: '这是作者',
+        bookName: '这是书名',
+        isNotFix: 'false'
+      }, {
+        title: '这是标题',
+        href: 'href',
+        cover: 'http://124.70.153.221:9998/ftppic/2021/20210305100759585.jpg',
+        page: 5,
+        createTime: '2021-02-02',
+        author: '这是作者',
+        bookName: '这是书名',
+        isNotFix: 'false'
+      }, {
+        title: '这是标题',
+        href: 'href',
+        cover: 'http://124.70.153.221:9998/ftppic/2021/20210305100759585.jpg',
+        page: 5,
+        createTime: '2021-02-02',
+        author: '这是作者',
+        bookName: '这是书名',
+        isNotFix: 'false'
+      }, {
+        title: '这是标题',
+        href: 'href',
+        cover: 'http://124.70.153.221:9998/ftppic/2021/20210305100759585.jpg',
+        page: 5,
+        createTime: '2021-02-02',
+        author: '这是作者',
+        bookName: '这是书名',
+        isNotFix: 'false'
+      }] })
     // console.log('getList()', getList())
   })
 }
